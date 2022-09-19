ef678082fdf141de1643b2fa4d79771a6158b7ad
Jackelin Garcia <jackygarcia19905@gmail.com>
2022-09-19 08:50:34 -0500

    Semana_7_d2_trabajada como semana_4_d2

--- Semana_4_d2.R
+++ Semana_4_d2.R
@@ -17,6 +17,7 @@ Entrevista$Carrera <- as.factor(Entrevista$Carrera)
 levels(Entrevista$Carrera)
 Entrevista$Semestre <- as.factor(Entrevista$Semestre)
 levels(Entrevista$Semestre)
+Entrevista$oi_4a <- as.factor(Entrevista$oi_4a)
 
 #Función para transformar caracteres en fechas
 Entrevista$Fecha <- as.Date(Entrevista$Fecha, "%d/%m/%y")
@@ -59,3 +60,41 @@ prop.table(op)*100
 #¿Presentaste en otra facultad?
 of <- table(Entrevista$oi_2a)
 prop.table(of)*100
+
+# Estuviste inscrito en otra facultad
+oi_4 <- table(Entrevista$oi_4)
+oi_4 
+prop.table(oi_4)*100
+
+# ¿Donde estabas incrito?
+oi_4a <- table(Entrevista$oi_4a)
+oi_4a
+prop.table(oi_4a)*100
+
+# Convivencia 
+ef_1 <- table(Entrevista$ef_1)
+ef_1
+prop.table(ef_1)*100
+
+# Instalaciones de la facultad
+ins_1 <- table(Entrevista$ins_1)
+ins_1
+prop.table(ins_1)*100
+
+# Conocen su malla curricular
+ap_1 <- table(Entrevista$ap_1)
+ap_1
+prop.table(ap_1)*100
+
+# Tareas a tiempo
+ap_2 <- table(Entrevista$ap_2)
+ap_2
+prop.table(ap_2)*100
+
+# Atención en las clases
+ap_3 <- table(Entrevista$ap_3)
+ap_3
+prop.table(ap_3)*100
+
+# Guardar la BD con los datos nuevos en formato csv
+write.csv(Entrevista, "Encuesta_codificada.csv")
