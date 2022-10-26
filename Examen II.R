3dcd77bf2ebfa3e3a1ef674836a50e5c24ab1a26
Jackelin Garcia <jackygarcia19905@gmail.com>
2022-10-26 09:25:32 -0500

    EXAMEN II

--- Examen II.R
+++ Examen II.R
@@ -10,14 +10,11 @@ tapply(InsectSprays$count, InsectSprays$spray, mean)
 tapply(InsectSprays$count, InsectSprays$spray, median)
 tapply(InsectSprays$count, InsectSprays$spray, sd)
 tapply(InsectSprays$count, InsectSprays$spray, var)
-# 1_ 6 tipos de insecticidas diferentes
 tapply(InsectSprays$count, InsectSprays$spray, length)
 
 boxplot(InsectSprays$count ~ InsectSprays$spray)
 insecticida <- shapiro.test
 "shapiro.test" (InsectSprays$count)
-# 2_ 12 observaciones c/u
-# 3_ 72 datos
 
 bartlett.test(InsectSprays$count, InsectSprays$spray)
 par.aov <- aov(InsectSprays$count ~ InsectSprays$spray)
